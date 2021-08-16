a3471a0d8c1e295d104799a289d7176d55b02c88
Ari Morales <ari.morales@travelwifi.com>
2021-08-16 16:34:47 -0500

    Initial Commit

---
+++ prepare.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+echo "Preparing folder init and creating ./init/initdb.sql"
+rm -rf ./init
+mkdir ./init
+#mkdir -p ./nginx/ssl >/dev/null 2>&1
+chmod -R 777 ./init
+docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --mysql > ./init/initdb.sql
+echo "done"
+chmod -R 777 ./init
+echo "done"
+#echo "Creating SSL certificates"
+#openssl req -nodes -newkey rsa:2048 -new -x509 -keyout ./nginx/ssl/self-ssl.key -out ./nginx/ssl/self.cert -subj '/C=DE/ST=BY/L=Hintertupfing/O=Dorfwirt/OU=Theke/CN=www.createyourown.domain/emailAddress=docker@createyourown.domain'
+#echo "You can use your own certificates by placing the private key in nginx/ssl/self-ssl.key and the cert in nginx/ssl/self.cert"
